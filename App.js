fa4b42dc4598d8bfd1273b2b6196ab307d780f9f
Yogesh <yogeshmore044@gmail.com>
2020-10-17 17:03:37 +0530

    Remove (Delete functionality)

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, TextInput, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
@@ -15,10 +15,37 @@ export default function App() {
 	const [taskList, setTaskList] = useState([]);
 
 	const onPressHandler = () => {
-		setTaskList(taskList => [...taskList, inputText]);
+		setTaskList(taskList => {
+			return [
+				...taskList,
+				{
+					id: Math.random().toString(),
+					value: inputText
+				}]
+		});
+
+		// console.log(taskList);
+
+		// data = [
+		// 	{
+		// 		id: '1',
+		// 		value:'Task1' 
+		// 	},
+		// 	{
+		// 		id:'2',
+		// 		value:'Task2'
+		// 	}
+		// ];
+
+
 		setInputText('');
 		// console.log('I am clicked');
 		// console.log(taskList);
+	}
+
+	const removeTask = (taskId) => {
+		// console.log('remove able task is ',taskId);
+		setTaskList(taskList => taskList.filter((item) => item.id !== taskId));
 	}
 	return (
 		<View style={design.container}>
@@ -31,13 +58,36 @@ export default function App() {
 				</View>
 			</View>
 
-			{taskList.map((value, index) => {
+			{/* {taskList.map((value, index) => {
 				return (
 					<View style={design.taskListHolder} key={index}>
 						<Text style={design.task}>{value}</Text>
 					</View>
 				);
-			})}
+			})} */}
+			{/* <FlatList
+				data={taskList}
+				renderItem={() => {
+					return (
+						<TouchableOpacity activeOpacity="0.5" style={design.taskListHolder} onPress={() => console.log('I am clicked')}>
+							<Text style={design.task}>Demo</Text>
+						</TouchableOpacity>
+					);
+				}}
+			/> */}
+
+			<FlatList
+				data={taskList}
+				renderItem={(myTask) => {
+					return (
+						<TouchableOpacity activeOpacity="0.2" style={design.taskListHolder} onLongPress={() => {
+							removeTask(myTask.item.id)
+						}}>
+							<Text style={design.task}>{myTask.item.value}</Text>
+						</TouchableOpacity>
+					);
+				}}
+			/>
 			<StatusBar style="auto" />
 		</View>
 	);
@@ -58,7 +108,7 @@ const design = StyleSheet.create({
 		borderWidth: 1,
 		borderColor: 'black',
 		padding: 7,
-		marginRight:5
+		marginRight: 5
 	},
 	btn: {
 		flex: 1
